b693161cd96b43a928d780d959a239890f5fbf9c
Mohammed <mohdnkh211016@gmail.com>
2025-08-28 15:50:50 +0300

    fix: set VITE_API_URL and adjust vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,9 @@
-const { defineConfig } = require('vite')
-const react = require('@vitejs/plugin-react')
-const path = require('path')
-
-module.exports = defineConfig({
+export default defineConfig({
   base: './',
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
